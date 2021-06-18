d9bf10603b73148db9734764176f5a80d0bc716a
Saurav-GCP <sauravshyju@gmail.com>
2021-06-18 13:20:55 +0000

    Another reset

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyPage extends StatefulWidget {
 class _MyPageState extends State<MyPage> {
   TextEditingController _textcontroller=TextEditingController();
   bool _secureText=true;
+  String _errorMsg='';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,8 +32,10 @@ class _MyPageState extends State<MyPage> {
 
 
                 obscureText: _secureText,
+
                 decoration: InputDecoration(
                   hintText: "Enter Password",
+                  errorText: _errorMsg,
                   labelText:"Password",
                   hintStyle: TextStyle(color: Colors.greenAccent),
                   labelStyle: TextStyle(color: Colors.red),
@@ -49,7 +52,24 @@ class _MyPageState extends State<MyPage> {
                   },)
 
                 ),
-              )
+              ),
+              SizedBox(
+                height: 80,
+              ),
+
+              ElevatedButton(onPressed: (){
+
+                setState(() {
+                  print(_textcontroller.text);
+                  if(_textcontroller.text.length<6){
+                    _errorMsg="Password too short";
+                  }
+                  else{
+                    _errorMsg='';
+                  }
+                });
+
+              }, child: Text("Click Here!"))
             ],
           ),
         )
