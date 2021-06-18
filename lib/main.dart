11597aeb5afaa72696f4f451a97d6f230a29a1f5
Saurav-GCP <sauravshyju@gmail.com>
2021-06-18 13:01:14 +0000

    Reset files, stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,59 @@
 import 'package:flutter/material.dart';
-
-void main() {
+void main()
+{
   runApp(MaterialApp(
-    home: MyApp(),
-  ));
+      home: MyPage()
+  )
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyPage extends StatefulWidget {
+  const MyPage({Key? key}) : super(key: key);
 
+  @override
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  TextEditingController _textcontroller=TextEditingController();
+  bool _secureText=true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        widget_fn(4, "0xff8D43B3"),
-        Padding(padding: EdgeInsets.all(3.0)),
-        Expanded(
-          flex: 4,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+        body: SafeArea(
+          child: Column(
             children: [
-              widget_fn(5, "0xff2AA650"),
-              Padding(padding: EdgeInsets.all(3.0)),
-              Expanded(
-                  flex: 5,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      widget_fn(2, "0xff58AAE8"),
-                      Padding(padding: EdgeInsets.all(3.0)),
-                      widget_fn(8, "0xffE73E33"),
-                    ],
-                  )),
+              SizedBox(
+                height: 80,
+              ),
+              TextField(
+                controller: _textcontroller ,
+
+
+                obscureText: _secureText,
+                decoration: InputDecoration(
+                  hintText: "Enter Password",
+                  labelText:"Password",
+                  hintStyle: TextStyle(color: Colors.greenAccent),
+                  labelStyle: TextStyle(color: Colors.red),
+                  border: OutlineInputBorder(),
+                  fillColor: Colors.white,
+                  filled: true,
+                  prefixIcon: Icon(Icons.security),
+                  suffixIcon: IconButton(icon: Icon(
+                    _secureText ? Icons.visibility_off : Icons.visibility
+                  ), onPressed: () {
+                    setState(() {
+                      _secureText= !_secureText;
+                    });
+                  },)
+
+                ),
+              )
             ],
           ),
-        ),
-        Padding(padding: EdgeInsets.all(3.0)),
-        widget_fn(2, "0xfffff900")
-      ],
-    ));
+        )
+    );
   }
 }
 
-Widget widget_fn(int flx, String colorval) {
-  return (Expanded(
-      flex: flx,
-      child: Container(
-        color: Color(int.parse(colorval)),
-        child: Center(
-          child: Text("#$colorval",
-              style: TextStyle(color: Colors.white, fontSize: 15),
-              textAlign: TextAlign.center),
-        ),
-      )));
-}
