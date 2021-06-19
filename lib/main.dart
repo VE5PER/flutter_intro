c05a5b4b6b4df25ae172657e5c1645030a75637e
Saurav-GCP <sauravshyju@gmail.com>
2021-06-19 13:39:14 +0000

    bg change

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
             children: [
