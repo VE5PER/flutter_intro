7af25334d0f2ad0256ff48f7c621d770cf70308d
Saurav-GCP <sauravshyju@gmail.com>
2021-07-01 13:31:33 +0000

    Stack widget

--- lib/multi.dart
+++ lib/multi.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyPage()));
@@ -10,121 +11,36 @@ class MyPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String value1 = '';
-    String value2 = '';
     return Scaffold(
       appBar: AppBar(
         title: Text("Base"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            onChanged: (text) {
-              value1 = text;
-            },
+      body: Center(
+        child: Container(
+          width: 400,
+          height: 700,
+          color: Colors.grey,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+           // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.red,
+              ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.blue,
+                ),
+              )
+            ],
           ),
-          TextField(
-            onChanged: (text) {
-              value2 = text;
-            },
-          ),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => MyPage2(
-                              value1: value1,
-                              value2: value2,
-                            )));
-              },
-              child: Text("New Stateless Page"),
-            ),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => MyPage3(value: value1)));
-              },
-              child: Text("New Stateful Page"),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MyPage2 extends StatelessWidget {
-  final value1, value2;
-  const MyPage2({Key? key, @required this.value1, @required this.value2})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("New Stateless Page"),
-        backgroundColor: Colors.red,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(value1),
-          Text(value2),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text("Pop page"),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MyPage3 extends StatefulWidget {
-  final value;
-  const MyPage3({Key? key, @required this.value}) : super(key: key);
-
-  @override
-  _MyPage3State createState() => _MyPage3State(value);
-}
-
-class _MyPage3State extends State<MyPage3> {
-  String value = '';
-  _MyPage3State(this.value);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("New Stateful Page"),
-        backgroundColor: Colors.red,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(value),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text("Pop page"),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
