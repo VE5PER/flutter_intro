f5f0a474eb5989954acab8b0e39e44c52c9f236b
Saurav-GCP <sauravshyju@gmail.com>
2021-07-01 13:04:28 +0000

    Passing values to stateful widget.

--- lib/multi.dart
+++ lib/multi.dart
@@ -31,7 +31,21 @@ class MyPage extends StatelessWidget {
                     MaterialPageRoute(
                         builder: (context) => MyPage2(value: value)));
               },
-              child: Text("New Page"),
+              child: Text("New Stateless Page"),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => MyPage3(value: value)));
+              },
+              child: Text("New Stateful Page"),
             ),
           ),
         ],
@@ -48,7 +62,43 @@ class MyPage2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("New"),
+        title: Text("New Stateless Page"),
+        backgroundColor: Colors.red,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(value),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Pop page"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyPage3 extends StatefulWidget {
+  final value;
+  const MyPage3({Key? key, @required this.value}) : super(key: key);
+
+  @override
+  _MyPage3State createState() => _MyPage3State(value);
+}
+
+class _MyPage3State extends State<MyPage3> {
+  String value = '';
+  _MyPage3State(this.value);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New Stateful Page"),
         backgroundColor: Colors.red,
       ),
       body: Column(
